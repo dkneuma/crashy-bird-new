67d7b8fa6c1a08f30f4ad575cd13aa5d36f3df24
Dan Neumann, PhD <dkneuma@users.noreply.github.com>
2024-02-13 10:09:15 -0500

    Update pxt.json, main.ts, main.blocks, main.py

--- main.py
+++ main.py
@@ -1,52 +1,59 @@
-def normalizeSprite():
-    for i in range(2):
-        if bird[i] < 0:
-            bird[i] = 0
-        if bird[i] > 7:
-            bird[1] = 7
-def createSprite(xval: number, yval: number):
-    bird.insert_at(0, xval)
-    bird.insert_at(1, yval)
+def spriteCreate(xval: number, yval: number, sprite: List[number]):
+    sprite.insert_at(0, xval)
+    sprite.insert_at(1, yval)
     display.set_matrix_color(xval, yval, GAME_ZIP64.colors(ZipLedColors.RED))
+    display.show()
+    return sprite
 
 def on_button_pressed_a():
     display.clear()
-    spriteChange("y", -1)
+    spriteChange("y", -1, bird)
     display.show()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def spriteChange(text: str, num: number):
+def spriteChange(text: str, num: number, sprite2: List[number]):
     # Check to make sure sprite stays in range 0 - 7
-    display.set_matrix_color(bird[0], bird[1], GAME_ZIP64.colors(ZipLedColors.BLACK))
+    display.set_matrix_color(sprite2[0],
+        sprite2[1],
+        GAME_ZIP64.colors(ZipLedColors.BLACK))
     if text == "x":
-        bird[0] = bird[0] + num
+        sprite2[0] = sprite2[0] + num
     else:
-        bird[1] = bird[1] + num
-    normalizeSprite()
-    display.set_matrix_color(bird[0], bird[1], GAME_ZIP64.colors(ZipLedColors.RED))
+        sprite2[1] = sprite2[1] + num
+    normalizeSprite(sprite2)
+    display.set_matrix_color(sprite2[0], sprite2[1], GAME_ZIP64.colors(ZipLedColors.RED))
     display.show()
 
 def on_button_pressed_b():
     display.clear()
-    spriteChange("y", 1)
+    spriteChange("y", 1, bird)
     display.show()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def spriteSet(coord: str, value: number):
+def spriteSet(coord: str, value: number, sprite3: List[number]):
     # Check to make sure sprite stays in range 0 - 7
-    display.set_matrix_color(bird[0], bird[1], GAME_ZIP64.colors(ZipLedColors.BLACK))
+    display.set_matrix_color(sprite3[0],
+        sprite3[1],
+        GAME_ZIP64.colors(ZipLedColors.BLACK))
     if coord == "x":
-        bird.insert_at(0, value)
+        sprite3.insert_at(0, value)
     else:
-        bird.insert_at(1, value)
-    normalizeSprite()
-    display.set_matrix_color(bird[0], bird[1], GAME_ZIP64.colors(ZipLedColors.RED))
+        sprite3.insert_at(1, value)
+    normalizeSprite(sprite3)
+    display.set_matrix_color(sprite3[0], sprite3[1], GAME_ZIP64.colors(ZipLedColors.RED))
     display.show()
+
+def normalizeSprite(sprite4: List[number]):
+    for i in range(2):
+        if sprite4[i] < 0:
+            sprite4[i] = 0
+        if sprite4[i] > 7:
+            sprite4[i] = 7
+    return sprite4
 bird: List[number] = []
 display: GAME_ZIP64.ZIP64Display = None
 obstacles: List[number] = []
 display = GAME_ZIP64.create_zip64_display()
 display.set_brightness(10)
 display.clear()
-bird = [0, 0]
-createSprite(0, 4)
+bird = spriteCreate(0, 4, [0, 0])
--- main.ts
+++ main.ts
@@ -1,57 +1,67 @@
-function normalizeSprite () {
-    for (let i = 0; i <= 1; i++) {
-        if (bird[i] < 0) {
-            bird[i] = 0
-        }
-        if (bird[i] > 7) {
-            bird[1] = 7
-        }
-    }
-}
-function createSprite (xval: number, yval: number) {
-    bird.insertAt(0, xval)
-    bird.insertAt(1, yval)
+function spriteCreate(xval: number, yval: number, sprite: number[]): number[] {
+    sprite.insertAt(0, xval)
+    sprite.insertAt(1, yval)
     display.setMatrixColor(xval, yval, GAME_ZIP64.colors(ZipLedColors.Red))
+    display.show()
+    return sprite
 }
-input.onButtonPressed(Button.A, function () {
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     display.clear()
-    spriteChange("y", -1)
+    spriteChange("y", -1, bird)
     display.show()
 })
-function spriteChange (text: string, num: number) {
-    // Check to make sure sprite stays in range 0 - 7
-    display.setMatrixColor(bird[0], bird[1], GAME_ZIP64.colors(ZipLedColors.Black))
+function spriteChange(text: string, num: number, sprite2: number[]) {
+    //  Check to make sure sprite stays in range 0 - 7
+    display.setMatrixColor(sprite2[0], sprite2[1], GAME_ZIP64.colors(ZipLedColors.Black))
     if (text == "x") {
-        bird[0] = bird[0] + num
+        sprite2[0] = sprite2[0] + num
     } else {
-        bird[1] = bird[1] + num
+        sprite2[1] = sprite2[1] + num
     }
-    normalizeSprite()
-    display.setMatrixColor(bird[0], bird[1], GAME_ZIP64.colors(ZipLedColors.Red))
+    
+    normalizeSprite(sprite2)
+    display.setMatrixColor(sprite2[0], sprite2[1], GAME_ZIP64.colors(ZipLedColors.Red))
     display.show()
 }
-input.onButtonPressed(Button.B, function () {
+
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     display.clear()
-    spriteChange("y", 1)
+    spriteChange("y", 1, bird)
     display.show()
 })
-function spriteSet (coord: string, value: number) {
-    // Check to make sure sprite stays in range 0 - 7
-    display.setMatrixColor(bird[0], bird[1], GAME_ZIP64.colors(ZipLedColors.Black))
+function spriteSet(coord: string, value: number, sprite3: number[]) {
+    //  Check to make sure sprite stays in range 0 - 7
+    display.setMatrixColor(sprite3[0], sprite3[1], GAME_ZIP64.colors(ZipLedColors.Black))
     if (coord == "x") {
-        bird.insertAt(0, value)
+        sprite3.insertAt(0, value)
     } else {
-        bird.insertAt(1, value)
+        sprite3.insertAt(1, value)
     }
-    normalizeSprite()
-    display.setMatrixColor(bird[0], bird[1], GAME_ZIP64.colors(ZipLedColors.Red))
+    
+    normalizeSprite(sprite3)
+    display.setMatrixColor(sprite3[0], sprite3[1], GAME_ZIP64.colors(ZipLedColors.Red))
     display.show()
 }
-let bird: number[] = []
-let display: GAME_ZIP64.ZIP64Display = null
-let obstacles: number[] = []
+
+function normalizeSprite(sprite4: number[]): number[] {
+    for (let i = 0; i < 2; i++) {
+        if (sprite4[i] < 0) {
+            sprite4[i] = 0
+        }
+        
+        if (sprite4[i] > 7) {
+            sprite4[i] = 7
+        }
+        
+    }
+    return sprite4
+}
+
+let bird : number[] = []
+let display : GAME_ZIP64.ZIP64Display = null
+let obstacles : number[] = []
 display = GAME_ZIP64.createZIP64Display()
 display.setBrightness(10)
 display.clear()
-bird = [0, 0]
-createSprite(0, 4)
+bird = spriteCreate(0, 4, [0, 0])
